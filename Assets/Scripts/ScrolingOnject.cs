409234d086cc2e538a452a67106f0b315af381ab
rappong <55365884+rappong@users.noreply.github.com>
2021-04-06 11:48:03 +0900

    0406

---
+++ Assets/Scripts/ScrolingOnject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrolingOnject : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
